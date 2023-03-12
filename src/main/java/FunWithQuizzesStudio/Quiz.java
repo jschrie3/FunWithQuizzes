23bf467323f2765b0a26eb02cd3cb9e97aaa9338
julian <julianschriedel@gmail.com>
2023-03-11 22:02:43 -0600

    building quiz class

--- src/main/java/FunWithQuizzesStudio/Quiz.java
+++ src/main/java/FunWithQuizzesStudio/Quiz.java
@@ -1,4 +1,8 @@
 package FunWithQuizzesStudio;
 
+import java.util.ArrayList;
+
 public class Quiz {
+    // create an empty arraylist so the questions can be added later
+    private final ArrayList<Question> questions = new ArrayList<>();
 }
